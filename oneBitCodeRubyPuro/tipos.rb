1e55d7c874cca0c6ac309638f3b0be6a7f42b07c
lisboamab <lisboamab@gmail.com>
2022-10-15 14:50:28 -0300

    gets e to_i

--- oneBitCodeRubyPuro/tipos.rb
+++ oneBitCodeRubyPuro/tipos.rb
@@ -1,3 +1,21 @@
-meudict = {nome:'Marcos', sobrenome: 'Lisboa'}
+#meudict = {nome:'Marcos', sobrenome: 'Lisboa'}
 
-puts meudict[:nome]
+#puts meudict[:nome]
+
+print 'Digite seu nome: '
+
+name = gets.chomp
+
+puts "Hello #{name}!"
+
+print "Digite o primeiro número: "
+
+numero1 = gets.chomp.to_i
+
+print "Digite o segundo número: "
+
+numero2 = gets.chomp.to_i
+
+soma = numero1 + numero2
+
+puts "A soma dos numeros #{numero1} e #{numero2} é igual a #{soma}"
