b64fd914ed1ea5f7ae8377a2ad322ce2dcb4e942
lisboamab <lisboamab@gmail.com>
2022-10-21 13:57:37 -0300

    Conclui o desafio 1 hash

---
+++ oneBitCodeRubyPuro/missaohash1.rb
@@ -0,0 +1,16 @@
+teste = Hash.new
+chave = ''
+valor = ''
+3.times do
+    puts "Escreva a chave: "
+    chave = gets.chomp
+
+    puts "Escreva o valor: "
+    valor = gets.chomp
+
+    teste[chave] = valor
+end 
+
+teste.each do |k, v|
+    puts "A chave #{k} tem o valor #{v}"
+end
