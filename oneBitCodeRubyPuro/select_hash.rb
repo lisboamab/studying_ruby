0f17e76ee0bfa0ae72828cc58638e6e45cec397f
lisboamab <lisboamab@gmail.com>
2022-10-21 13:25:33 -0300

    adiciona select hash e array

---
+++ oneBitCodeRubyPuro/select_hash.rb
@@ -0,0 +1,10 @@
+hash = {0 => 'zero', 1 => 'um', 2 => 'dois', 3 => 'tres'}
+
+puts 'Selecionando keys com valor maiores que 1'
+
+selection_key = hash.select do |key, value|
+    key > 1
+end
+
+puts selection_key
+
