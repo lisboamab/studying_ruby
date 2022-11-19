4da21c79186f76c4676a034208f4c354cebda583
lisboamab <lisboamab@gmail.com>
2022-11-19 13:27:17 -0300

    revisando estudos

--- oneBitCodeRubyPuro/array.rb
+++ oneBitCodeRubyPuro/array.rb
@@ -1,11 +1,31 @@
 estados = ["Ceará", "São Paulo", "Rio de Janeiro", "Minas Gerais", "Rio Grande do Norte", "Maranhão"]
 
+#coloca sempre no final
 estados.push("Bahia")
 
-estados.push("Acre", "Amapá")
+#coloca na posição indicada por um número inteiro
+estados.insert(0, "Acre", "Amapá")
 
 #deleta um item com index especifico
 estados.delete_at(0)
 
 #deleta o ultimo elemento diferente do python
 estados.pop
+
+#em intervalos usando [numero .. numero]
+puts estados
+puts "-----------------------"
+puts estados[2..4]
+
+#conta os elementos
+puts estados.count
+
+#pega o primeiro e o ultimo elemento
+puts estados.first
+puts estados.last
+
+#retorna se o array está vazio com bool
+puts estados.empty?
+
+#consulta se elemento x está no array
+puts estados.include?('São Paulo')
--- oneBitCodeRubyPuro/hash.rb
+++ oneBitCodeRubyPuro/hash.rb
@@ -1,16 +1,31 @@
-capitais = {}
-#ou capitais = Hash.new
+=begin
+
+Os metodos empty? e include?() também funcionam no tipo hash
+
+=end
+
+capitais = {} #ou capitais = Hash.new
+
 
 capitais = {acre: 'Rio Branco', ceara: 'Fortaleza', rio_grande_do_norte: 'Natal'}
 
+#inserindo novo valor
+capitais[:rio_de_janeiro] = 'Rio de Janeiro'
+
+#exibindo um valor especifico
 puts capitais[:acre]
 
+#ver todas as chaves
 puts capitais.keys
 
+#ver todos os valores
 puts capitais.values
 
+#retorna o um int com o tamanho do hash
 puts capitais.size
 
+#deletar um elemento
 capitais.delete(:acre)
 
-puts capitais.size
+#checando o tipo
+puts capitais.class
